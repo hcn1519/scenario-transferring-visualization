d42c69ca0c571935c2a29508f512ff82f8a36b19
changnam hong <hcn1519@gmail.com>
2024-03-16 15:24:10 -0700

    Import base objects and sample data

---
+++ openscenario_msgs/compile_proto.sh
@@ -0,0 +1,4 @@
+protoc \
+  -I=/home/runner/ADS-scenario-transfer-1/ \
+  -I=/home/runner/ADS-scenario-transfer-1/openscenario_msgs \
+   --python_out=/home/runner/ADS-scenario-transfer-1/ /home/runner/ADS-scenario-transfer-1/openscenario_msgs/*.proto
